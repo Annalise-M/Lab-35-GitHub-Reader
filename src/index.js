ee65725c122c1d01a6b15b2ecf888676b92a561e
Annalise Murphy <nali247@gmail.com>
2020-11-03 14:58:22 -0800

    current skeleton set

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import gitHub from './gitHub';
 import App from './components/app/App';
 
 render(
-  <App />,
+  <Provider gitHub={gitHub}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/reducers/userReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  users: [],
+  error: null
+};
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
