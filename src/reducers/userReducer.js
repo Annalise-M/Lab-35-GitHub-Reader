8752e972d6eece6061cce2cdb1c20eef599da293
Annalise Murphy <nali247@gmail.com>
2020-11-03 16:01:29 -0800

    test and userReducer updated and passing

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,10 +1,14 @@
+import { SET_USERS } from '../actions/userActions';
+
 const initialState = {
-  users: [],
+  profile: {},
   error: null
 };
 
 export default function reducer(state = initialState, action) {
   switch(action.type) {
+    case SET_USERS:
+      return { ...state, profile: action.payload };
     default:
       return state;
   }
--- src/reducers/userReducer.test.js
+++ src/reducers/userReducer.test.js
@@ -4,15 +4,33 @@ import reducer from './userReducer';
 describe('user reducer', () => {
   it('handles the SET_USERS action', () => {
     const state = {
-      users: [],
+      profile: [
+        { login: 'annalise-m' },
+        { html_url: 'https://github.com/Annalise-M' },
+        { followers: '9' },
+        { following: '16' }
+      ],
       error: null
     };
 
     const action = setUsers([
       { login: 'annalise-m' },
+      { html_url: 'https://github.com/Annalise-M' },
+      { followers: '9' },
+      { following: '16' }
     ]);
 
     const newState = reducer(state, action);
+
+    expect(newState).toEqual({
+      profile: [
+        { login: 'annalise-m' },
+        { html_url: 'https://github.com/Annalise-M' },
+        { followers: '9' },
+        { following: '16' }
+      ],
+      error: null
+    });
   });
 
 });
